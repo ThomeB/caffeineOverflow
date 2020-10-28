eba7640fe7253e0b88ac4437b555e930a90b22e2
ThomeB <branden.thome@gmail.com>
2020-10-28 12:53:22 -0600

    added same variables

--- Driver.java
+++ Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
 
 	public static void main(String[] args){
+		int testV = 1;
+		int testV2 = 2;
 		System.out.println("Jake's questions are dumb!");
 		System.out.println("Branden's questions are dumb!");
 		System.out.println("Jake's a freaking cow!");
