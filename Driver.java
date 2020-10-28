05e348511d607bbbb92cb830db8cdc92eb450b28
ThomeB <branden.thome@gmail.com>
2020-10-28 12:48:40 -0600

    added two variables

--- Driver.java
+++ Driver.java
@@ -2,6 +2,8 @@ public class Driver{
 
 	public static void main(String[] args)
 	{
+		int testV = 1;
+		int testV2 = 2;
 		System.out.println("Jake's questions are dumb!");
 		System.out.println("Branden's questions are dumb!");
 <<<<<<< HEAD
