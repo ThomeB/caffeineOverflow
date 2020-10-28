fd4e01320dbbfef72db8b1499b5692c2d007c952
Jake Rhoads <62158166+Jrhoads32@users.noreply.github.com>
2020-10-28 12:29:56 -0600

    Added Evan in there too!

--- Driver.java
+++ Driver.java
@@ -1,8 +1,10 @@
 public class Driver{
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 		System.out.println("Jake's questions are dumb!");
 		System.out.println("Branden's questions are dumb!");
+		System.out.println("Evan's questions are dumb!");
 	}
 
 
