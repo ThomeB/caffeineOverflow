f85066e2562f8191b7ef6e6ec73db0ac9b124ad4
Evan Pickett <pickett7474@hotmail.com>
2020-11-04 10:48:57 -0800

    Delete addedClass.java

--- addedClass.java
+++
@@ -1,7 +0,0 @@
-public class addedClass{
-	/*
-	 * Made this outside of eclipse - added it to my git file and hit refresh
-	 * then it popped up in my working tree
-	 * committed this to dev
-	 */
-}
